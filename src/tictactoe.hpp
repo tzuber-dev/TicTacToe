5ef136269217d39434cdabf47168b35fa8a94f2f
tzuber-dev <zt50481@mail.harpercollege.edu>
2026-03-04 14:31:53 -0600

    Updated .hpp file.

--- src/tictactoe.hpp
+++ src/tictactoe.hpp
@@ -1,3 +1,45 @@
 #include <string>
 
-// Your functions go here
+
+
+class TTTMechanics {
+public:
+
+    TTTMechanics();
+
+    void RestartGamePhase();
+
+    char GetPlayerLive() const;
+
+    bool MoveUnit(int cellrange);
+
+    bool WinnerGotten() const;
+
+    char EquipWinner() const;
+
+
+    bool Draw() const;
+
+
+    bool DoneGame() const;
+
+
+    std::string BoardString() const;
+
+
+private:
+
+    char boardgrid[9];
+
+    char presentplayer;
+
+    char victoriousplayer;
+
+    void WinnerStatusLive();
+
+    bool CellGone(int indx) const;
+
+
+
+
+};
